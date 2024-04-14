9d1946ebccb274a8a247518fe9cd55458d2f36c2
iamsimon <simonrema123@gmail.com>
2024-04-14 09:08:07 +0530

    "checking done"

--- backend/controllers/assignmentFunctions.js
+++ backend/controllers/assignmentFunctions.js
@@ -1,5 +1,6 @@
 const Assignment = require("../models/assignmentModel"); // Assuming your user model is exported as User
-const fetchTextData = require ("../functions/fetchTextFromUrl.js");
+const {fetchTextData,compareStrings} = require ("../functions/fetchTextFromUrl.js");
+const Axios = require('axios');
 
 class AssigmentController {
         async addNewAssignment(assignment) {
@@ -28,17 +29,37 @@ class AssigmentController {
                 const submissions=currAssignment.submissions
                  const testCases=currAssignment.testCases
                  const language=currAssignment.language
-
+const testCaseSize=testCases.length
+var newSubmissions=submissions;
                 for (const submission of submissions)
                 {
                     const file=submission.file
-                    const convertedFile=fetchTextData(file)
+                    const convertedFile=await fetchTextData(file)
+                    var a=0.00;
                     for(const testCase of testCases)
                     {
-                       const response=await this.checkCode({code:convertedFile,language:language,input:testCase.input,output:testCase.output})
-                       console.log(response)
+
+                       
+                        const response=await this.checkCode({code:convertedFile,language:language,input:testCase.input,output:testCase.output})
+                        console.log(response+" "+testCase.output)
+                      if(compareStrings(response,testCase.output))
+                      {a++;
+                        console.log("correct")
+                      }
+
+
                     }
+                    var marks=(a/testCaseSize)*100;
+                    newSubmissions=newSubmissions.filter(msg=>msg.rollNumber!==submission.rollNumber)
+var nextSubmission=submission
+nextSubmission.marks=marks;
+newSubmissions.push(nextSubmission)
+                    console.log(marks)
                 }
+                currAssignment.submissions=newSubmissions
+                await currAssignment.save()
+
+                
 
     
                 //await newAssignment.save();
@@ -53,7 +74,7 @@ class AssigmentController {
                 let code = details.code; 
                 let language = details.language; 
                 let input = details.input; 
-              
+              console.log(code+" "+ language+" "+ input)
                 if (language === "python") { 
                     language = "python3"
                 } 
@@ -75,20 +96,12 @@ class AssigmentController {
                     data : data3
                   };
 
-               await Axios(options) 
-                 .then((response) => { 
-                    console.log(response.data) 
-
-                       return response.data
-                    }).catch((error) => { 
-                        console.log(error); 
-
-                        return error.message
-                    }); 
-
+                  const response = await Axios(options);
+                  // Return the output if the request was successful
+                  
+                  return response.data.output;
              
                 
-                return ;
             } catch (err) {
                 throw new Error(err);
             }
--- backend/functions/fetchTextFromUrl.js
+++ backend/functions/fetchTextFromUrl.js
@@ -3,8 +3,16 @@ const fetch=require('node-fetch')
 async function fetchTextData(url) {
     const response = await fetch(url);
     const textData = await response.text();
-    console.log(textData);
     return textData;
 }
 
-module.exports = fetchTextData;
+function compareStrings(str1, str2) {
+    // Remove all spaces and newlines from both strings
+    const strippedStr1 = str1.replace(/\s/g, '');
+    const strippedStr2 = str2.replace(/\s/g, '');
+
+    // Compare the stripped strings
+    return strippedStr1 === strippedStr2;
+}
+
+module.exports = {fetchTextData,compareStrings};
